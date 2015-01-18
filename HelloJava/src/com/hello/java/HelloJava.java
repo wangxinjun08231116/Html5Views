9ad4ee5ab8fa0d64f9342c5aef41b9f99c4518c7
xinjunwang <xinjunwang@xinjunwangdeMacBook-Pro.local>
2015-01-18 15:32:45 +0800

    ok

---
+++ HelloJava/src/com/hello/java/HelloJava.java
@@ -0,0 +1,14 @@
+package com.hello.java;
+
+public class HelloJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+
+	}
+
+}
